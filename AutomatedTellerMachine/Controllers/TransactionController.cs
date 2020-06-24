94f34292a108aa4e634b35c15fb773e4f65afdb1
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-24 07:13:14 +0530

    Add project files.

---
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using AutomatedTellerMachine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Deposit(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Deposit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
